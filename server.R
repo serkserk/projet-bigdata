6a3ffe7d06a3bdfaccf344ed013c098c12d53e5b
killianghx <Killian.guiheux@ig2h.com>
2018-04-28 17:32:50 +0200

    modification of the app

--- server.R
+++ server.R
@@ -3,6 +3,8 @@
 ######################### LIBRARY ############################
 
 library(mongolite)
+library(leaflet)
+library(dplyr)
 
 ################### CONNECTION TO MONGO #######################
 
@@ -22,10 +24,52 @@ convertDateForMongo <- function(date){
   return (strftime(date , "%Y-%m-%dT%H:%M:%SZ"))
 }
 
+# Chargement des coordonnées gps des capteurs
+loadcapteurs <- function(){
+  
+  capteurs = cap$find(fields = '{ "geometry.coordinates": 1, "fields.id_arc_tra" : 1}')
+  idscapteurs <- capteurs %>% select(fields,geometry) %>% distinct(fields,.keep_all = TRUE)
+  idscapteurs <- na.omit(idscapteurs)
+  
+  idscapteurs$lng = sapply(idscapteurs$geometry$coordinates, 
+                           function(e) { 
+                             if (!is.null(e)) return(e[1]); 
+                             return(NA) 
+                           })
+  
+  idscapteurs$lat = sapply(idscapteurs$geometry$coordinates, 
+                           function(e) { 
+                             if (!is.null(e)) return(e[2]); 
+                             return(NA) 
+                           })
+  
+  geo <- data.frame(idscapteurs$fields$id_arc_tra,idscapteurs$lng,idscapteurs$lat)
+  geo <- na.omit(geo)
+  geo$idscapteurs.fields.id_arc_tra <- NULL
+  rownames(geo) <- geo$idscapteurs.fields.id_arc_tra
+  colnames(geo) <- c("lng","lat")
+  
+  return(geo)
+  
+}
+
 ################### SERVER ##################
 
 shinyServer( function(input, output,session) { 
   
+  # Carte de Paris
+  paris = leaflet() %>% addTiles %>% setView(lng = 2.34, lat = 48.855, zoom = 12) %>% addTiles()
+  
   print("lancement de l'application")
+  
+  # carte des capteurs
+  
+  output$Carte_capteurs <- renderLeaflet({
+    
+    loadcapteurs()
+    geo <- loadcapteurs()
+    paris %>% addCircleMarkers(data = geo, radius = 1)
+    
+  })
   
 })
--- ui.R
+++ ui.R
@@ -1,15 +1,25 @@
 ####### Shiny application UI #######
 
+# CREATED BY : Killian Guiheux, Serkan Azap, Corentin Guillo
+
 library(shiny)
 library(leaflet)
 
 
 
 ui <- fluidPage(
+  
   tabsetPanel(type = "tabs",
-              mainPanel(
-                titlePanel("Titre de la page"),
-              plotOutput(outputId = "histogram",width = "100%")
+              
+              sidebarLayout(
+                sidebarPanel(
+                  plotOutput(outputId = "histogram",width = "100%",height = "280px")
+                ),
+                
+                mainPanel(
+                  titlePanel("Carte des capteurs"),
+                  leafletOutput(outputId = "Carte_capteurs")
+              )
       )
   )
 )
